6fb61ca7ae98a75698f9edd008d385073b34d9f9
RAGNAROSaa <williamhao@sjtu.edu.cn>
2016-11-26 10:19:56 +0800

    删除了最初的mysite3

--- mysite3/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from account import views
-
- 
-urlpatterns = patterns('',
-    url(r'^$', views.register, name='register'),
-    url(r'^register/$',views.register,name = 'register'),
-)
